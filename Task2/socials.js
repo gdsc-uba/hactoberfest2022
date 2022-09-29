ec10bd72d3fcab20f5f75d2a7c0fa82095877aec
Steve Yonkeu <yokwejuste@yahoo.com>
2022-09-29 09:31:32 +0100

    feat: added yokwejuste socials

---
+++ Task2/socials.js
@@ -0,0 +1,8 @@
+[
+    yokwejuste_socials = {
+        "twitter_handle": "yokwejuste",
+        "likedin_handle": "yokwejuste",
+        "facebook": "yokwejuste5013",
+        "telegram_handle": "googledevz"
+    },
+]

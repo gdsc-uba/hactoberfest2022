23fccfd778b7aa789e1f9e0e037082041c12f8a0
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-09-30 13:42:43 +0530

    feat: added iamhariom0228 socials

---
+++ Task2/socials.js
@@ -0,0 +1,9 @@
+[
+    iamhariom0228_socials = {
+        "twitter_handle": "yokwejuste",
+        "likedin_handle": "yokwejuste",
+        "facebook": "yokwejuste5013",
+        "telegram_handle": "googledevz"
+    }
+    
+]

0f2381b26936980dae4841f24565eb9c45008ed8
imyogeshgaur <yogeshgaur1995@gmail.com>
2022-10-01 05:27:09 +0530

    feat: added imyogeshgaur socials

---
+++ Task2/socials.js
@@ -0,0 +1,8 @@
+[
+    imyogeshgaur_socials = {
+        "twitter_handle": "imyogeshgaur",
+        "likedin_handle": "imyogeshgaur",
+        "facebook": " ",
+        "telegram_handle": "imyogeshgaur"
+    }
+]

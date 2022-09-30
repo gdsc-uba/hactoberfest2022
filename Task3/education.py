c6c435cbbda992e395133d415504fc26caa2ff87
Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>
2022-09-30 14:08:24 +0530

    feat: added educational background of iamhariom0228

---
+++ Task3/education.py
@@ -0,0 +1,5 @@
+[
+    ["ABV IIITM Gwalior", "B.Tech in Computer Science", "2021", "present"],
+    ["Public School Darbhanga", "Higher Secondary Education", "2019", "2021"],
+    ["Public School Darbhanga", "Secondary Education", "2014", "2019"]
+]

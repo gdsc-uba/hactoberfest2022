09fc97266307928e8f3bcf259248a67981873383
rupali-codes <rupali7487@gmail.com>
2022-09-30 11:53:01 +0530

    feat: added rupali-codes to socials

---
+++ Task2/socials.js
@@ -0,0 +1,8 @@
+[
+    rupali_codes_socials = {
+        "twitter_handle": "rupali_codes",
+        "likedin_handle": "rupali-codes",
+        "telegram_handle": "rupali_codes",
+        "instagram": "rupali_codes"
+    }
+]

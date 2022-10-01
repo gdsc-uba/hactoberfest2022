9b5ac7543cfdb76c945d374b1b63be87191ceaf4
AntishK <kdhant01099@gmail.com>
2022-10-01 23:27:19 +0400

    feat: added all tasks - AntishK

--- Task3/education.py
+++ Task3/education.py
@@ -40,4 +40,5 @@
     ["Vivekanand Education Society Institute of Technology","Bachelor of Engineering","2020","present"],
     ["University Ninth of July","Bachelor of Engineering","2017","2021"],
     ["university of Buea", "Faculty of Engineering and Technology", "2020", "present"],
+    ["University Of Mauritius", "Faculty Of Information Communication and Digital Technologies", "2018", "2021"],
 ]

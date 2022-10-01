7ad0304827cef2a65d83cccbe79dcd061b0bbdf5
Abongwa Bonalais <81782853+Burnleydev1@users.noreply.github.com>
2022-10-01 11:16:23 +0100

    feat: Added education details burnleydev1

--- Task3/education.py
+++ Task3/education.py
@@ -13,5 +13,7 @@
     ["State Institute of engineering and Technology ", "Nilokheri", "2018", "present"],
     ["Ahmadu Bello University", "Bachelor of Engineering", "2018", "present"],
     ["University of Bamenda", "Nahpi", "2019", "present"],
-    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
+    ["university of bamenda", "Nahpi", "2019", "present"],
+
 ]

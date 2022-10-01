a0a3b4648d39ce0fcd2e65247bc48cdac2b17b27
hrishikesh-g <hrishikesh.ghule2308@gmail.com>
2022-10-01 17:25:48 +0530

    feat: added educational background of hrishikesh-g

--- Task3/education.py
+++ Task3/education.py
@@ -20,5 +20,6 @@
     ["University of Bamenda", "Nahpi", "2019", "present"],
     ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
     ["university of bamenda", "Nahpi", "2019", "present"],
-    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"], 
+    ["Government College of Engineering Nagpur, Computer Science", "Bachelor of Technology", "2020", "present"],       
 ]

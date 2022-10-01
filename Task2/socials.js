76005341db01351653692f22fcc187275ba075f9
shubh045 <shubh6441@gmail.com>
2022-10-01 11:03:47 +0530

    feat: added shubh045 socials

---
+++ Task2/socials.js
@@ -0,0 +1,8 @@
+[
+    shubh045_socials = {
+        "twitter_handle": "shubhM_4",
+        "linkedin_handle": "shubham-1045ba219",
+        "facebook": "",
+        "telegram_handle": "Shubh_am_05"
+    }
+]

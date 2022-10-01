56a4e11053ee5f6d3d242da2cf929fcd37d202c7
MR-DHRUV <dhruvgupta742@gmail.com>
2022-10-01 16:22:37 +0530

    feat: added educational background of MR-DHRUV

--- Task3/education.py
+++ Task3/education.py
@@ -17,5 +17,5 @@
     ["University of Bamenda", "Nahpi", "2019", "present"],
     ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
     ["university of bamenda", "Nahpi", "2019", "present"],
-
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
 ]

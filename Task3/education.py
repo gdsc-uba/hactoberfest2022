7d2a71ba1b3e822af289a548cf46957edea1d64f
NishaChawla18 <98702810+NishaChawla18@users.noreply.github.com>
2022-10-01 16:58:32 +0530

    Update education.py

--- Task3/education.py
+++ Task3/education.py
@@ -20,5 +20,6 @@
     ["University of Bamenda", "Nahpi", "2019", "present"],
     ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
     ["university of bamenda", "Nahpi", "2019", "present"],
-    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],    
+    ["jc bose university of science and technology","Bachelor of Technology","2021","present"],
 ]

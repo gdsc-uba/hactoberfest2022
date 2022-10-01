f93a4e706639291cde9cbd062e9b7aaee65527c4
Hari Om Sharma <89256126+iamhariom0228@users.noreply.github.com>
2022-10-01 12:25:11 +0530

    Task2 (#26)
    * feat: added iamhariom0228 socials
    
    * feat: added iamhariom0228 socials
    
    Co-authored-by: Hariom Sharma <89256126+Hariom-Starlink90@users.noreply.github.com>

---
+++ Task2/socials.js
@@ -0,0 +1,7 @@
+iamhariom0228_socials = {
+    "twitter_handle": "i_am_hariom_",
+    "likedin_handle": "hari-om-sharma-72249b244",
+    "instagram": "hari_om_sharma",
+    "telegram_handle": "iamhariom0228"
+}
+    

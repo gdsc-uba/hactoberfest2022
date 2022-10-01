fbce8737da6d00306aac47e08062760f018533b9
Petra <81535499+PetraAG@users.noreply.github.com>
2022-10-01 08:05:55 -0400

    Update education.py

--- Task3/education.py
+++ Task3/education.py
@@ -20,5 +20,6 @@
     ["University of Bamenda", "Nahpi", "2019", "present"],
     ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
     ["university of bamenda", "Nahpi", "2019", "present"],
-    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],        
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],
+    ["University of bamenda, Bachelor of software engineering", "COLTECH", "2021", "present"],
 ]

049073fbfdb41cdee1731a7ebe3162c69b7d5c3c
SHUBHAM <84277638+shubh045@users.noreply.github.com>
2022-10-01 12:37:10 +0530

    feat: added shubh045 socials (#51)
    Co-authored-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- Task2/socials.js
+++ Task2/socials.js
@@ -1,7 +1,14 @@
-iamhariom0228_socials = {
-    "twitter_handle": "i_am_hariom_",
-    "likedin_handle": "hari-om-sharma-72249b244",
-    "instagram": "hari_om_sharma",
-    "telegram_handle": "iamhariom0228"
-}
-    
+[
+   shubh045_socials = {
+        "twitter_handle": "shubhM_4",
+        "linkedin_handle": "shubham-1045ba219",
+        "facebook": "",
+        "telegram_handle": "Shubh_am_05"
+    },
+  iamhariom0228_socials = {
+      "twitter_handle": "i_am_hariom_",
+      "likedin_handle": "hari-om-sharma-72249b244",
+      "instagram": "hari_om_sharma",
+      "telegram_handle": "iamhariom0228"
+  }
+]

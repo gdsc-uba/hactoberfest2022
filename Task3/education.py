d79da8f41a577e084644ed14284635760ec894c9
Nishant Ola <nishant.4716403221@ipu.ac.in>
2022-10-01 15:32:46 +0530

    feat: added educational background of ola-nishant

--- Task3/education.py
+++ Task3/education.py
@@ -12,4 +12,5 @@
     ["university of bamenda", "Nahpi", "2019", "present"],
     ["State Institute of engineering and Technology ", "Nilokheri", "2018", "present"],
     ["Ahmadu Bello University", "Bachelor of Engineering", "2018", "present"],
+    ["University School of Information, Communication and Technology", "Bachelor of Technology", "2021", "present"],
 ]

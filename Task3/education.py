bba695b540c9e595b4c7925b48cd87d7f9683774
Ngumih Fien <72187339+Fienne@users.noreply.github.com>
2022-10-01 10:02:25 +0100

    feat: add educational background of Fienne (#70)
    Co-authored-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- Task3/education.py
+++ Task3/education.py
@@ -9,5 +9,6 @@
     ["Chandigarh College of Engineering and Technology", "B.E. in Computer Science", "2020", "present"],
     ["university of mauritius", "Computer Science", "2018", "2021"],
     ["university of bamenda", "Nahpi", "2019", "present"],
+    ["university of bamenda", "Nahpi", "2019", "present"],
     ["State Institute of engineering and Technology ", "Nilokheri", "2018", "present"],
 ]

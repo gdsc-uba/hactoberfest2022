27786bd5fb873763bb8ba2e95dc65702bb164d39
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-10-02 09:03:20 +0100

    feat: added Edmond22-prog socials

---
+++ Task2/socials.js
@@ -0,0 +1,8 @@
+[
+    Edmond22prog_socials = {
+        "twitter_handle": "MakolleG",
+        "linkedin_handle": "Edmond Ghislain Makolle",
+        "facebook": "",
+        "telegram_handle": "Edghi_Makol"
+    }
+]

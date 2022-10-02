9b633f61749835241173fd524b9ceaece753e6bb
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2022-10-02 08:19:24 +0530

    Update education.py

--- Task3/education.py
+++ Task3/education.py
@@ -40,4 +40,5 @@
     ["Vivekanand Education Society Institute of Technology","Bachelor of Engineering","2020","present"],
     ["University Ninth of July","Bachelor of Engineering","2017","2021"],
     ["university of Buea", "Faculty of Engineering and Technology", "2020", "present"],
+    ["SLIIT", "Faculty of Computing", "2019", "present"],
 ]

9c67764f31145b8782a9f176f2ca1dafa69da4a8
Bembenk <putrabayu0180@gmail.com>
2022-10-04 23:10:17 +0700

    feat: added educational background of bembenk18

--- Task3/education.py
+++ Task3/education.py
@@ -40,4 +40,5 @@
     ["Vivekanand Education Society Institute of Technology","Bachelor of Engineering","2020","present"],
     ["University Ninth of July","Bachelor of Engineering","2017","2021"],
     ["university of Buea", "Faculty of Engineering and Technology", "2020", "present"],
+    ["university of Indonesia", "Communication and Technology", "2020", "present"],
 ]

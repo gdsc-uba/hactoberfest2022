83b97c1b1ce8a01ce1a435eacead9f62005602b0
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2022-10-04 20:47:37 +0530

    feat: added educational background of <PRABHU-OFFICIAL>

--- Task3/education.py
+++ Task3/education.py
@@ -40,4 +40,5 @@
     ["Vivekanand Education Society Institute of Technology","Bachelor of Engineering","2020","present"],
     ["University Ninth of July","Bachelor of Engineering","2017","2021"],
     ["university of Buea", "Faculty of Engineering and Technology", "2020", "present"],
+    ["Siksha O' Anusandhan Deemed to Be University", "Computer Science And Engineering", "2020-24", "present"]
 ]

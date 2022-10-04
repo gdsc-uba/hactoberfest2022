d4efc1970a2efdd90183e8f7a281fad921162834
Shraddha Gulati <hemandeepgulati@gmail.com>
2022-10-05 02:36:51 +0530

    feat: added gshraddha_ socials

--- Task2/socials.js
+++ Task2/socials.js
@@ -120,22 +120,22 @@ dibyajittripathy_socials = {
     "facebook": "agienpetra ",
     "telegram_handle": "PDoc",
 },
-{
+
   VaibhavArora314_socials = {
     "twitter_handle": "",
     "likedin_handle": "Vaibhav Arora",
     "facebook": "Vaibhav Arora",
     "telegram_handle": "vaibhav"
-  }
-},	
-{
+  },
+
+
 impetus007_socials = {
   "twitter_handle": "Vishal_it_is",
   "likedin_handle": "impetus",
   "facebook": "vishal2219singh",
   "telegram_handle": "",
 },
-lando-paul = {
+lando_paul = {
   "twitter_handle": "nasty_poems",
   "likedin_handle": "lando-momo-paul-junior-76b413229",
   "facebook": "",
@@ -170,7 +170,7 @@ adrzxxx_socials= {
   "likedin_handle": "aditya-singh-04a74b19a",
   "facebook": "",
   "telegram_handle": ""
-  }
+  },
 MahirJain_socials= {
   "twitter_handle": "hrishikesh123T",
   "likedin_handle": "Mahir Jain",
@@ -215,4 +215,13 @@ AchaRhaah_socials= {
   "instagram": "Acha Rha'ah",
   "facebook" : "Acha Rha'ah",
 }, 
+
+ShraddhaGulati_socials = {
+  "linkedin_handle": "Shraddha Gulati",
+  "instagram": "gshraddha_",
+  "codeforces": "gshraddha_",
+  "facebook": "Shraddha Gulati",
+  "discord": "gshraddha_"
+},
+
 ] 

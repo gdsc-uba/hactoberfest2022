04b6f4ff85b2a9ebb41b4c42dbe27cf5e005c38f
Okafor Daniel <dancentcee38@gmail.com>
2022-10-07 04:09:07 +0100

    feat: added educational background of <dancentcee>
    clear

--- Task3/education.py
+++ Task3/education.py
@@ -40,4 +40,5 @@
     ["Vivekanand Education Society Institute of Technology","Bachelor of Engineering","2020","present"],
     ["University Ninth of July","Bachelor of Engineering","2017","2021"],
     ["university of Buea", "Faculty of Engineering and Technology", "2020", "present"],
+    ["Nnamdi Azikiwe University", "Bachelor of Education", "2017", "present"]
 ]

e1232402c94d663bd6de28984031732b6163f928
mayuras7685 <mayurasodara@gmail.com>
2022-10-15 20:38:10 +0530

    feat: added mayuras7685 socials

--- Task2/socials.js
+++ Task2/socials.js
@@ -170,7 +170,7 @@ adrzxxx_socials= {
   "likedin_handle": "aditya-singh-04a74b19a",
   "facebook": "",
   "telegram_handle": ""
-  }
+  },
 MahirJain_socials= {
   "twitter_handle": "hrishikesh123T",
   "likedin_handle": "Mahir Jain",
@@ -214,5 +214,11 @@ AchaRhaah_socials= {
   "telegram_handle": "@Acha Rha'ah",
   "instagram": "Acha Rha'ah",
   "facebook" : "Acha Rha'ah",
+  }, 
+mayuras7685_socials= {
+  "twitter_handle": "MayurAsodara",
+  "likedin_handle": "Mayur Asodara",
+  "instagram": "mayur_7685",
+  
 }, 
 ] 

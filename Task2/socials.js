bbee1a833337f578b7433561803c467468558af9
Shrishti20706 <87308489+Shrishti20706@users.noreply.github.com>
2022-10-18 00:23:00 +0530

    Update socials.js

--- Task2/socials.js
+++ Task2/socials.js
@@ -215,4 +215,9 @@ AchaRhaah_socials= {
   "instagram": "Acha Rha'ah",
   "facebook" : "Acha Rha'ah",
 }, 
+ Shrishti20706_socials= {
+  "twitter_handle": "@Ci3Jain",
+  "likedin_handle": "Shrishti Jain",
+  "telegram_handle": "Shrishti Jain",
+}
 ] 

6ad1f6c9b01937a9a087cb4f60c6045be33d9596
Pritam Bhattacherjee <95472088+PritamBhattacherjee@users.noreply.github.com>
2022-10-18 21:06:32 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+# Creating a Dictionary
+# with dict() method
+Dict = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+# Creating a Dictionary
+# with each item as a Pair
+Dict = dict([(1, 'Geeks'), (2, 'For')])
+print("\nDictionary with each item as a pair: ")
+print(Dict)

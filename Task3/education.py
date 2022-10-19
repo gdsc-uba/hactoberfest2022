fb089ff46d1d4dfe99bbd2b5582e5cc98d3399ff
shivvamm <shivam574488@gmail.com>
2022-10-19 21:07:01 +0530

    feat: added educational background of <shivvamm>

--- Task3/education.py
+++ Task3/education.py
@@ -1,4 +1,4 @@
-[
+[   ["Abdul kalam Technical University", "B.Tech Computer Science", "2024", "present"],
     ["University of Engineering and Management Jaipur", "B.Tech Computer Science", "2020", "present"],
     ["Krishna Engineering College", "Bachelor Of Technology", "2018", "2022"],
     ["The ICT University", "Bachelor of Science in Computer Science", "2020", "present"],

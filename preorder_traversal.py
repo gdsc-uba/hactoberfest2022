1f018113afbfed434c2e589f675897226a11582f
Devanshu Srivastava <42748021+devanshusrivastava25@users.noreply.github.com>
2022-10-20 17:11:59 +0530

    Create preorder_traversal.py

---
+++ preorder_traversal.py
@@ -0,0 +1,39 @@
+# Python3 program to for tree traversals
+ 
+# A class that represents an individual node in a
+# Binary Tree
+ 
+ 
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+ 
+ 
+# A function to do preorder tree traversal
+def printPreorder(root):
+ 
+    if root:
+ 
+        # First print the data of node
+        print(root.val),
+ 
+        # Then recur on left child
+        printPreorder(root.left)
+ 
+        # Finally recur on right child
+        printPreorder(root.right)
+ 
+ 
+# Driver code
+if __name__ == "__main__":
+  root = Node(1)
+  root.left = Node(2)
+  root.right = Node(3)
+  root.left.left = Node(4)
+  root.left.right = Node(5)
+ 
+  # Function call
+  print "Preorder traversal of binary tree is"
+  printPreorder(root)

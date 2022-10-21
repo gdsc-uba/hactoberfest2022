83a4b1345cfb85474618eed7234a417506ad0c52
Asra Mushtaq <115940183+asragithubit@users.noreply.github.com>
2022-10-21 18:47:58 +0500

    Swapping two numbers without using third variable

---
+++ Swapping without using temporary variable.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int first_number;
+//	int second_number;
+//	//Getting two numbers from user
+//	cout << "Enter First Number : ";
+//	cin >> first_number;
+//	cout << "Enter Second Number : ";
+//	cin >> second_number;
+//	
+//	first_number = first_number + second_number;//first_number has sum of both of first_number & second_number now
+//
+//	second_number = first_number - second_number;  //second_number now has difference of first_number(sum of first & second number) and second_number
+//
+//	first_number = first_number - second_number;
+//
+//	cout << "After Swapping : " << endl;
+//	cout << "First Number : " << first_number;
+//	cout << endl;
+//
+//	cout << "Second Number : " << second_number;
+//	cout << endl;
+//
+//
+//
+//}
